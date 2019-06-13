06cfec83a821e48f6ac54fdb518eb6ecda5bc96c
Meredith <dinsdale.meredith@gmail.com>
2019-06-13 00:46:31 -0700

    react components structured for sign in, log in, and home page

---
+++ client/src/components/Home.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
+import Nav from './Nav';
+import MyTrips from './MyTrips';
+
+class Home extends Component {
+
+ render () {
+     return (
+        <div>
+            <div>
+                 <Nav />      
+             </div>
+
+             <div className='container'>
+                <div className='row homeBanner jumbotron'>
+                    <div class='col'>
+                        <h2 className='headline'>Tripsy Home Page</h2>
+                    </div>
+                </div>
+
+                <div className='row'>
+                    <div className='col'>      
+                        <Link to='/newtrip'><button className='btn'>New Trip</button></Link>
+                    </div>
+                </div>
+
+                <div className='row myTripsDiv'>
+                    <MyTrips />
+                </div>     
+
+            </div>
+        </div>
+     )
+    }
+}
+export default Home;
---
+++ client/src/components/MyTrips.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class MyTrips extends Component {
+
+    render() {
+        return (
+            <h6>My Trips Render Here :)</h6>
+        )
+    }
+}
+export default MyTrips;
