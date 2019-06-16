b9e188c322ced9e7a8270d606fd70da89f3eb62f
RLC <rlcarmody@gmail.com>
2019-06-15 17:15:25 -0700

    new message fires event with tripid and message

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 //@ts-check
 const express = require('express');
+const socketIO = require('socket.io');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const cookieParser = require('cookie-parser');
 
 const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true}));
@@ -18,8 +20,18 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/tripsy', { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/tripsy', { useNewUrlParser: true, useCreateIndex: true })
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
+
+const io = socketIO(server);
+
+app.set('io', io);
+
+io.on('connect', socket => {
+  console.log('successfully connected to socket')
+})
+
+
