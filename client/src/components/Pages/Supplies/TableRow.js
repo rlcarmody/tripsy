382d8d028a7b028c8faac9e6bab7e06c829a5d1b
Meredith <dinsdale.meredith@gmail.com>
2019-06-25 21:26:02 -0700

    null

--- client/src/components/Pages/Supplies/TableRow.js
+++ client/src/components/Pages/Supplies/TableRow.js
@@ -5,22 +5,22 @@ class TableRow extends Component {
     super(props);
   }
 
-render() {
-  return (
-    <Fragment>
-      <tr>
-        <td>
-          {item}
-        </td>
-        <td>
-          {claimedBy}
-        </td>
-        <td>
-          <button onClick={() => this.props.onClaimed(this.props.id)} type="button">Claim</button>
-        </td>
-      </tr>
-    </Fragment>
-  );
-}
+  render() {
+    return (
+      <Fragment>
+        <tr>
+          <td>
+            {item}
+          </td>
+          <td>
+            {claimedBy}
+          </td>
+          <td>
+            <button onClick={() => this.props.onClaimed(this.props.id)} type="button">Claim</button>
+          </td>
+        </tr>
+      </Fragment>
+    );
+  }
 }
 export default TableRow;
